0e9b1dd150741fc6bf15ef44ab060505c88c338b
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2021-08-27 12:50:47 +0100

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://eddienko.github.com/owl-shell-pipeline",
-    version="0.2.1",
+    version="0.2.2",
     zip_safe=False,
     python_requires=">=3.7",
 )
