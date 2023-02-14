7fa528849909627c5e5c04d78d9579d6f91d7efc
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2023-02-14 17:36:55 +0000

    fup

--- shell_pipeline/schema.py
+++ shell_pipeline/schema.py
@@ -5,7 +5,7 @@ import voluptuous as vo
 schema = vo.Schema(
     vo.All(
         {
-            vo.Required("version"): vo.In([2.0]),
+            vo.Required("version"): vo.In(['2.0']),
             vo.Optional("output_path"): vo.Coerce(Path),
             vo.Optional("input_path"): vo.Coerce(Path),
             vo.Required("command"): str,
